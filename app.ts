169361511ea58283eaa7061afb19baa639408d32
Bouih32 <enamto.bouih@gmail.com>
2024-12-30 22:46:48 +0100

    build : setup typescript and prisma

--- app.ts
+++ app.ts
@@ -1,12 +1,28 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import prisma from "./utils/prisma";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 7856;
 
 app.use(express.json());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.post("/", async (req: Request, res: Response) => {
+  try {
+    await prisma.user.create({
+      data: {
+        email: "othmane@gmail.com",
+        userName: "AMLO",
+        firstName: "othmane",
+        lastName: "bouih",
+        password: "aaaaa",
+        role: "bigboss",
+      },
+    });
+    res.status(200).send({ message: "wach nta wa7d lafan" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "aw aw aw ach tatrowen" });
+  }
 });
 
 app.listen(port, () => {
