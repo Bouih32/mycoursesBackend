3c61c8673765b8f52795f5965c161e9c951de249
Bouih32 <enamto.bouih@gmail.com>
2025-01-01 19:46:55 +0100

    style: remove coments

--- app.ts
+++ app.ts
@@ -9,8 +9,8 @@ dotenv.config();
 const port = process.env.PORT || 7856;
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
   standardHeaders: "draft-8",
 });
 
